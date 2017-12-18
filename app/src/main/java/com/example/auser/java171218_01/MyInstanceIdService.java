d75f85549b9eed86c24b90686843ae3b78c938db
AC <ragnarokdoom@gmail.com>
2017-12-18 16:33:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/java171218_01/MyInstanceIdService.java
@@ -0,0 +1,26 @@
+package com.example.auser.java171218_01;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+/**
+ * Created by auser on 2017/12/18.
+ */
+
+public class MyInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+}
